41bc2479a55dada2eb028a8fb63d541b1aec7ec5
Jose Jimenez <jijimenezf@hotmail.com>
2025-01-09 12:39:17 -0600

    Update to recent versions for Next, React and React DOM in order to avoid vulnerabilities
    - Added output standalon in configuration for optimizing the build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",  // For optimizing Docker builds
 };
 
 export default nextConfig;
